3fe3003e66f322e6bb83533dfafbfc844273f60a
Philippe Gonzalez <philgzl@outlook.com>
2022-04-12 17:05:08 +0200

    Stop saving metadata

--- wsj0_convert.py
+++ wsj0_convert.py
@@ -1,7 +1,5 @@
 import argparse
-import json
 import os
-import re
 import subprocess
 
 
@@ -19,71 +17,62 @@ def main():
     if not os.path.exists(audio_dir):
         os.mkdir(audio_dir)
 
-    # initialize metadata dictionary
-    metadata = {}
+    # find all wv1 files
+    wv1_files = []
+    for root, folders, files in os.walk(args.input):
+        for file in filter(lambda x: x.endswith('wv1'), files):
+            wv1_files.append(os.path.join(root, file))
+    n_files = len(wv1_files)
 
     # main loop
-    for root, folders, files in os.walk(args.input):
-        for file in filter(lambda x: x.endswith(('wv1', 'wv2')), files):
-            filepath = os.path.join(root, file)
+    for i, filepath in enumerate(wv1_files):
 
-            # parse metadata from file header
-            with open(filepath, 'rb') as f:
-                header = f.read(1024).decode("utf-8")
-            m = re.findall(r"([\S]+?) -[\S]+? (.+?)\n", header)
-            if not m:
-                raise ValueError(f'could not parse header in {filepath}')
-            file_metadata = dict(m)
+        # show progress
+        if not args.quiet:
+            print(f'{i}/{n_files} ({i/n_files*100:.1f}%)')
 
-            # use the utterance id as key
-            utterance_id = file_metadata.pop('utterance_id')
-            metadata[utterance_id] = file_metadata
+        # grab utterance id and speaker id and from filename
+        utterance_id = os.path.basename(filepath).replace('.wv1', '.wav')
+        speaker_id = utterance_id[:3]
+        speaker_dir = os.path.join(audio_dir, speaker_id)
+        if not os.path.exists(speaker_dir):
+            os.mkdir(speaker_dir)
 
-            # grab speaker id and create speaker directory
-            speaker_id = file_metadata['speaker_id']
-            speaker_dir = os.path.join(audio_dir, speaker_id)
-            if not os.path.exists(speaker_dir):
-                os.mkdir(speaker_dir)
-
-            # check is output file already exists
-            wavfile = os.path.join(speaker_dir, f'{utterance_id}.wav')
-            flacfile = wavfile.replace('.wav', '.flac')
-            if args.no_flac:
-                outfile = wavfile
-            else:
-                outfile = flacfile
-            if os.path.exists(outfile):
-                if not args.quiet:
-                    print(f'{outfile} already exists')
-                continue
-
-            # call sph2pipe to convert to wav
+        # check if output file already exists
+        wavfile = os.path.join(speaker_dir, utterance_id)
+        flacfile = wavfile.replace('.wav', '.flac')
+        if args.no_flac:
+            outfile = wavfile
+        else:
+            outfile = flacfile
+        if os.path.exists(outfile):
             if not args.quiet:
-                print(f'writing {wavfile}')
+                print(f'{outfile} already exists')
+            continue
+
+        # call sph2pipe to convert to wav
+        if not args.quiet:
+            print(f'writing {wavfile}')
+        subprocess.call([
+            'sph2pipe',
+            '-f',
+            'wav',
+            filepath,
+            wavfile,
+        ])
+
+        # call ffmpeg to convert to flac and delete wav file
+        if not args.no_flac:
+            if not args.quiet:
+                print(f'converting to {flacfile}')
             subprocess.call([
-                'sph2pipe',
-                '-f',
-                'wav',
-                filepath,
+                'ffmpeg',
+                '-y',
+                '-i',
                 wavfile,
-            ])
-
-            # call ffmpeg to convert to flac and delete wav file
-            if not args.no_flac:
-                if not args.quiet:
-                    print(f'converting to {flacfile}')
-                subprocess.call([
-                    'ffmpeg',
-                    '-y',
-                    '-i',
-                    wavfile,
-                    flacfile,
-                ], stderr=subprocess.DEVNULL)
-                os.remove(wavfile)
-
-    # save metadata
-    with open(os.path.join(args.input, 'metadata.json'), 'w') as f:
-        json.dump(metadata, f)
+                flacfile,
+            ], stderr=subprocess.DEVNULL)
+            os.remove(wavfile)
 
 
 if __name__ == '__main__':
